7699201f13fe008158e76c849091220c64dd7d61
Prashika-M <prashikamathi27@gmail.com>
2025-12-26 00:30:32 +0530

    Update 03_bottleneck_identification.sql

--- sql/03_bottleneck_identification.sql
+++ sql/03_bottleneck_identification.sql
@@ -1,1 +1,15 @@
+/* ============================================================
+   QUERY PURPOSE: REVIEW FINAL DRG BOTTLENECT RANKING
+   ============================================================ */
+SELECT *
+FROM drg_bottleneck_summary
+ORDER BY bottleneck_rank;
 
+/* ============================================================
+   QUERY PURPOSE: ISOLATE TOP BOTTLENECK DRGs
+   ============================================================ */
+SELECT
+    DRG_Desc
+INTO #top_bottleneckdrgs
+FROM drg_bottleneck_summary
+WHERE bottleneck_rank <= 5;
